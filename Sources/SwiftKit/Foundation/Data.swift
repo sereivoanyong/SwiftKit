c8ff3e50501bddb1a6d953660ef8e744306ef150
Sereivoan Yong <sereivoanyong@gmail.com>
2020-05-09 15:34:10 +0700

    Add data's mime type

--- Sources/SwiftKit/Foundation/Data.swift
+++ Sources/SwiftKit/Foundation/Data.swift
@@ -17,6 +17,29 @@ extension Data {
     }
   }
   
+  public var mimeType: String {
+    var b: UInt8 = 0
+    copyBytes(to: &b, count: 1)
+    switch b {
+    case 0xFF:
+      return "image/jpeg"
+    case 0x89:
+      return "image/png"
+    case 0x47:
+      return "image/gif"
+    case 0x4D, 0x49:
+      return "image/tiff"
+    case 0x25:
+      return "application/pdf"
+    case 0xD0:
+      return "application/vnd"
+    case 0x46:
+      return "text/plain"
+    default:
+      return "application/octet-stream"
+    }
+  }
+  
   public func jsonObject(options: JSONSerialization.ReadingOptions = []) throws -> Any {
     return try JSONSerialization.jsonObject(with: self, options: options)
   }
