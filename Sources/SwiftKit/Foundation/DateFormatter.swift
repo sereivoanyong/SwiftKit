573962f454f0db73c84333f75ba3e84f8cc4b211
Sereivoan Yong <sereivoanyong@gmail.com>
2020-05-15 11:04:15 +0700

    Update date formatter's convenient initializers

--- Sources/SwiftKit/Foundation/DateFormatter.swift
+++ Sources/SwiftKit/Foundation/DateFormatter.swift
@@ -9,24 +9,34 @@ import Foundation
 
 extension DateFormatter {
   
-  @inlinable public convenience init(dateStyle: Style, timeStyle: Style) {
+  @inlinable public convenience init(dateStyle: Style, timeStyle: Style, locale: Locale? = nil, timeZone: TimeZone? = nil, calendar: Calendar? = nil) {
     self.init()
     self.dateStyle = dateStyle
     self.timeStyle = timeStyle
+    self.locale = locale
+    self.timeZone = timeZone
+    self.calendar = calendar
   }
   
-  @inlinable public convenience init(dateFormat: String) {
+  @inlinable public convenience init(dateFormat: String, locale: Locale? = nil, timeZone: TimeZone? = nil, calendar: Calendar? = nil) {
     self.init()
     self.dateFormat = dateFormat
+    self.locale = locale
+    self.timeZone = timeZone
+    self.calendar = calendar
+  }
+  
+  @inlinable public convenience init?(localizedDateFormatFromTemplate template: String, locale: Locale? = nil, timeZone: TimeZone? = nil, calendar: Calendar? = nil) {
+    if let localizedDateFormat = Self.dateFormat(fromTemplate: template, options: 0, locale: locale) {
+      self.init(dateFormat: localizedDateFormat, locale: locale, timeZone: timeZone, calendar: calendar)
+    } else {
+      return nil
+    }
   }
   
   public static func iso8601(withFractionalSeconds: Bool) -> DateFormatter {
-    let dateFormatter = DateFormatter()
-    dateFormatter.calendar = Calendar(identifier: .iso8601)
-    dateFormatter.locale = Locale(identifier: "en_US_POSIX")
-    dateFormatter.timeZone = TimeZone(secondsFromGMT: 0)
-    dateFormatter.dateFormat = withFractionalSeconds ? "yyyy-MM-dd'T'HH:mm:ss.SSSXXXXX" : "yyyy-MM-dd'T'HH:mm:ssZZZZZ"
-    return dateFormatter
+    let dateFormat = withFractionalSeconds ? "yyyy-MM-dd'T'HH:mm:ss.SSSXXXXX" : "yyyy-MM-dd'T'HH:mm:ssZZZZZ"
+    return DateFormatter(dateFormat: dateFormat, locale: Locale(identifier: "en_US_POSIX"), timeZone: TimeZone(secondsFromGMT: 0), calendar: Calendar(identifier: .iso8601))
   }
 }
 #endif
