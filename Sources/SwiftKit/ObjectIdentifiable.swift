c2180343eb31eafd724442edcccf90ec4840de4a
Sereivoan Yong <sereivoanyong@gmail.com>
2020-06-02 17:45:38 +0700

    Add HashableObjectIdentifiable

--- Sources/SwiftKit/ObjectIdentifiable.swift
+++ Sources/SwiftKit/ObjectIdentifiable.swift
@@ -9,3 +9,18 @@ public protocol ObjectIdentifiable {
   associatedtype ID: Hashable
   var id: ID { get }
 }
+
+public protocol HashableObjectIdentifiable: ObjectIdentifiable, Hashable {
+  
+}
+
+extension HashableObjectIdentifiable {
+  
+  public static func == (lhs: Self, rhs: Self) -> Bool {
+    return lhs.id == rhs.id
+  }
+  
+  public func hash(into hasher: inout Hasher) {
+    hasher.combine(id)
+  }
+}
