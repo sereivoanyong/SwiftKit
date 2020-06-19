23e25c05b4eac70ae992676e9ed1dd97a3079641
Sereivoan Yong <sereivoanyong@gmail.com>
2020-06-19 14:19:15 +0700

    Add convenient bar button item's factory methods

--- Sources/SwiftKit/UIKit/UIBarButtonItem.swift
+++ Sources/SwiftKit/UIKit/UIBarButtonItem.swift
@@ -9,10 +9,6 @@ import UIKit
 
 extension UIBarButtonItem {
   
-  public convenience init(systemItem: SystemItem) {
-    self.init(barButtonSystemItem: systemItem, target: nil, action: nil)
-  }
-  
   final public var view: UIView? {
     get { return value(forKey: "view") as? UIView }
     set { setValue(newValue, forKey: "view") }
@@ -31,6 +27,14 @@ extension UIBarButtonItem {
     let barButtonItem = UIBarButtonItem(barButtonSystemItem: .fixedSpace, target: nil, action: nil)
     barButtonItem.width = width
     return barButtonItem
+  }
+  
+  @inlinable public static func system(item: SystemItem, target: Any?, action: Selector?) -> Self {
+    return Self(barButtonSystemItem: item, target: target, action: action)
+  }
+  
+  @inlinable public static func custom(view: UIView) -> Self {
+    return Self(customView: view)
   }
 }
 #endif
