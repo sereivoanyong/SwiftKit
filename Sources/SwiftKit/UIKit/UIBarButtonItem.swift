63eb3c14275206f6bb10f1c74291ec74229c0858
Sereivoan Yong <sereivoanyong@gmail.com>
2020-08-08 10:30:36 +0700

    Organize

--- Sources/SwiftKit/UIKit/UIBarButtonItem.swift
+++ Sources/SwiftKit/UIKit/UIBarButtonItem.swift
@@ -9,26 +9,6 @@ import UIKit
 
 extension UIBarButtonItem {
   
-  final public var view: UIView? {
-    get { return value(forKey: "view") as? UIView }
-    set { setValue(newValue, forKey: "view") }
-  }
-  
-  final public func set(target: AnyObject?, action: Selector?) {
-    self.target = target
-    self.action = action
-  }
-  
-  public static func flexibleSpace() -> Self {
-    return Self(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
-  }
-  
-  public static func fixedSpace(width: CGFloat) -> Self {
-    let barButtonItem = Self(barButtonSystemItem: .fixedSpace, target: nil, action: nil)
-    barButtonItem.width = width
-    return barButtonItem
-  }
-  
   @inlinable public convenience init(systemItem: SystemItem, target: AnyObject? = nil, action: Selector? = nil) {
     self.init(barButtonSystemItem: systemItem, target: target, action: action)
   }
@@ -38,6 +18,26 @@ extension UIBarButtonItem {
     self.title = title
     self.image = image
     self.style = style
+    self.target = target
+    self.action = action
+  }
+  
+  public static func fixedSpace(_ width: CGFloat) -> Self {
+    let barButtonItem = Self(barButtonSystemItem: .fixedSpace, target: nil, action: nil)
+    barButtonItem.width = width
+    return barButtonItem
+  }
+  
+  public static func flexibleSpace() -> Self {
+    return Self(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+  }
+  
+  final public var view: UIView? {
+    get { return value(forKey: "view") as? UIView }
+    set { setValue(newValue, forKey: "view") }
+  }
+  
+  final public func set(target: AnyObject?, action: Selector?) {
     self.target = target
     self.action = action
   }
