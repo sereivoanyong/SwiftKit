63262ded750b04ea33856b4585808cb9ee26e1fd
Sereivoan Yong <sereivoanyong@gmail.com>
2020-11-24 15:28:35 +0700

    Add static `attachment(_:)`, `attachment(data:uti:)`, & `attachment(image:)` to `NSAttributedString`

--- Sources/SwiftKit/UIKit/NSTextAttachment.swift
+++ Sources/SwiftKit/UIKit/NSTextAttachment.swift
@@ -14,4 +14,19 @@ extension NSTextAttachment {
     self.image = image
   }
 }
+
+extension NSAttributedString {
+  
+  public static func attachment(_ attachment: NSTextAttachment) -> NSAttributedString {
+    return NSAttributedString(attachment: attachment)
+  }
+  
+  public static func attachment(data: Data?, uti: String?) -> NSAttributedString {
+    return attachment(NSTextAttachment(data: data, ofType: uti))
+  }
+  
+  public static func attachment(image: UIImage?) -> NSAttributedString {
+    return attachment(NSTextAttachment(image: image))
+  }
+}
 #endif
