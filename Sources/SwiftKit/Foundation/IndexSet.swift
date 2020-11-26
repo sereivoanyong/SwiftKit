596c84875e5acc5e0d00ce99fbfcdd0b32a01d72
Sereivoan Yong <sereivoanyong@gmail.com>
2020-11-26 10:40:04 +0700

    Separate `IndexSet` from `RangeReplaceableCollection`

---
+++ Sources/SwiftKit/Foundation/IndexSet.swift
@@ -0,0 +1,29 @@
+//
+//  IndexSet.swift
+//
+//  Created by Sereivoan Yong on 11/26/20.
+//
+
+#if canImport(Foundation)
+import Foundation
+
+extension RangeReplaceableCollection where Index == IndexSet.Element {
+  
+  public subscript(indexSet: IndexSet) -> [Element] {
+    var collection: [Element] = []
+    for index in indexSet {
+      collection.append(self[index])
+    }
+    return collection
+  }
+  
+  @discardableResult
+  public mutating func remove(at indexSet: IndexSet) -> [Element] {
+    var removedElements: [Element] = []
+    for index in indexSet.reversed() {
+      removedElements.append(remove(at: index))
+    }
+    return removedElements
+  }
+}
+#endif
--- Sources/SwiftKit/Swift/RangeReplaceableCollection.swift
+++ Sources/SwiftKit/Swift/RangeReplaceableCollection.swift
@@ -36,27 +36,3 @@ extension RangeReplaceableCollection {
     insert(newElement, at: count + k)
   }
 }
-
-#if canImport(Foundation)
-import Foundation
-
-extension RangeReplaceableCollection where Index == IndexSet.Element {
-  
-  public subscript(indexSet: IndexSet) -> [Element] {
-    var collection: [Element] = []
-    for index in indexSet {
-      collection.append(self[index])
-    }
-    return collection
-  }
-  
-  @discardableResult
-  public mutating func remove(at indexSet: IndexSet) -> [Element] {
-    var removedElements: [Element] = []
-    for index in indexSet.reversed() {
-      removedElements.append(remove(at: index))
-    }
-    return removedElements
-  }
-}
-#endif
