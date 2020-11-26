dab380559e3ec280fd254fa50ac5481a4ff01a0f
Sereivoan Yong <sereivoanyong@gmail.com>
2020-11-26 10:56:27 +0700

    Clean up NSStringDrawing

--- Sources/SwiftKit/UIKit/NSStringDrawing.swift
+++ Sources/SwiftKit/UIKit/NSStringDrawing.swift
@@ -7,9 +7,9 @@
 #if canImport(UIKit)
 import UIKit
 
-// MARK: StringDrawing Bridge
-
 extension String {
+  
+  // MARK: StringDrawing
   
   public func size(withAttributes attributes: [NSAttributedString.Key: Any]) -> CGSize {
     return (self as NSString).size(withAttributes: attributes)
@@ -22,11 +22,8 @@ extension String {
   public func draw(in rect: CGRect, withAttributes attributes: [NSAttributedString.Key: Any]) {
     (self as NSString).draw(in: rect, withAttributes: attributes)
   }
-}
-
-// MARK: ExtendedStringDrawing Bridge
-
-extension String {
+  
+  // MARK: ExtendedStringDrawing
   
   public func draw(with rect: CGRect, options: NSStringDrawingOptions, attributes: [NSAttributedString.Key: Any], context: NSStringDrawingContext? = nil) {
     (self as NSString).draw(with: rect, options: options, attributes: attributes, context: context)
@@ -35,32 +32,11 @@ extension String {
   public func boundingRect(with size: CGSize, options: NSStringDrawingOptions, attributes: [NSAttributedString.Key: Any], context: NSStringDrawingContext? = nil) -> CGRect {
     return (self as NSString).boundingRect(with: size, options: options, attributes: attributes, context: context)
   }
-}
-
-extension NSAttributedString {
   
-  public func sizeThatFits(width: CGFloat?) -> CGSize {
-    if let width = width {
-      let height = boundingRect(with: CGSize(width: width, height: .greatestFiniteMagnitude), options: [.usesFontLeading, .usesLineFragmentOrigin], context: nil).height
-      return CGSize(width: width, height: height)
-    } else {
-      return size()
-    }
-  }
-}
-
-extension String {
-  
-  public func size(with font: UIFont) -> CGSize {
-    return size(withAttributes: [.font: font])
-  }
+  // MARK: Convenience
   
   public func sizeThatFits(_ size: CGSize, options: NSStringDrawingOptions = [.usesFontLeading, .usesLineFragmentOrigin], attributes: [NSAttributedString.Key: Any]) -> CGSize {
     return boundingRect(with: size, options: options, attributes: attributes).size
-  }
-  
-  public func sizeThatFits(_ size: CGSize, options: NSStringDrawingOptions = [.usesFontLeading, .usesLineFragmentOrigin], font: UIFont) -> CGSize {
-    return sizeThatFits(size, options: options, attributes: [.font: font])
   }
   
   public func sizeThatFits(width: CGFloat?, options: NSStringDrawingOptions = [.usesFontLeading, .usesLineFragmentOrigin], attributes: [NSAttributedString.Key: Any]) -> CGSize {
@@ -76,16 +52,23 @@ extension String {
     return sizeThatFits(CGSize(width: width, height: .greatestFiniteMagnitude), options: options, attributes: attributes).height
   }
   
-  public func heightThatFits(width: CGFloat, options: NSStringDrawingOptions = [.usesFontLeading, .usesLineFragmentOrigin], font: UIFont) -> CGFloat {
-    return heightThatFits(width: width, options: options, attributes: [.font: font])
-  }
-  
   public func heightThatFits(width: CGFloat, options: NSStringDrawingOptions, font: UIFont, limitedToNumberOfLines numberOfLines: Int) -> CGFloat {
     return min(font.lineHeight(numberOfLines: numberOfLines), heightThatFits(width: .greatestFiniteMagnitude, options: options, attributes: [.font: font]))
   }
 }
 
 extension NSAttributedString {
+  
+  final public func sizeThatFits(width: CGFloat?) -> CGSize {
+    if let width = width {
+      let height = boundingRect(with: CGSize(width: width, height: .greatestFiniteMagnitude), options: [.usesFontLeading, .usesLineFragmentOrigin], context: nil).height
+      return CGSize(width: width, height: height)
+    } else {
+      return size()
+    }
+  }
+  
+  // MARK: Convenience
   
   final public func sizeThatFits(_ size: CGSize, options: NSStringDrawingOptions = [.usesFontLeading, .usesLineFragmentOrigin]) -> CGSize {
     return boundingRect(with: size, options: options, context: nil).size
