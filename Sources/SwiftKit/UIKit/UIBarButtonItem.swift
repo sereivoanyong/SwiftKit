3526a44855d051f109ffd85ce471eb007fcdeca8
Sereivoan Yong <sereivoanyong@gmail.com>
2020-12-21 09:56:22 +0700

    Add handler support to `UIBarButtonItem`

--- Sources/SwiftKit/UIKit/UIBarButtonItem.swift
+++ Sources/SwiftKit/UIKit/UIBarButtonItem.swift
@@ -37,9 +37,49 @@ extension UIBarButtonItem {
     set { setValue(newValue, forKey: "view") }
   }
   
-  final public func set(target: AnyObject?, action: Selector?) {
+  final public func setTarget(_ target: AnyObject?, action: Selector?) {
     self.target = target
     self.action = action
+  }
+
+  // MARK: Action Support
+
+  public convenience init(systemItem: SystemItem, handler: ((UIBarButtonItem) -> Void)? = nil) {
+    self.init(systemItem: systemItem, target: nil, action: nil)
+    self.handler = handler
+  }
+
+  public convenience init(title: String? = nil, image: UIImage? = nil, style: Style = .plain, handler: ((UIBarButtonItem) -> Void)? = nil) {
+    self.init(title: title, image: image, style: style, target: nil, action: nil)
+    self.handler = handler
+  }
+
+  private static var actionKey: Void?
+  final public var handler: ((UIBarButtonItem) -> Void)? {
+    get {
+      if let action = associatedObject(forKey: &Self.actionKey) as Action<UIBarButtonItem>?, case .sender(let handler) = action.handler {
+        return handler
+      }
+      return nil
+    }
+    set {
+      if let newValue = newValue {
+        if let action = associatedObject(forKey: &Self.actionKey) as Action<UIBarButtonItem>? {
+          action.handler = .sender(newValue)
+        } else {
+          let action = Action<UIBarButtonItem>(handler: newValue)
+          setTarget(action, action: #selector(Action<UIBarButtonItem>.invoke(_:)))
+          setAssociatedObject(action, forKey: &Self.actionKey)
+        }
+      } else {
+        if let action = associatedObject(forKey: &Self.actionKey) as Action<UIBarButtonItem>? {
+          if self.target === action && self.action == #selector(Action<UIBarButtonItem>.invoke(_:)) {
+            setTarget(nil, action: nil)
+          }
+        }
+        setAssociatedObject(nil as Action<UIBarButtonItem>?, forKey: &Self.actionKey)
+      }
+    }
   }
 }
 #endif
