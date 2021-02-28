8ee6280a53e3c020a1cf816c68add7f43eb4dd2d
Sereivoan Yong <sereivoanyong@gmail.com>
2021-02-28 20:19:01 +0700

    Add instance `appleLanguages` to `UserDefaults`

--- Sources/SwiftKit/Foundation/UserDefaults.swift
+++ Sources/SwiftKit/Foundation/UserDefaults.swift
@@ -8,7 +8,12 @@
 import Foundation
 
 extension UserDefaults {
-  
+
+  final public var appleLanguages: [String] {
+    get { object(forKey: "AppleLanguages") as? [String] ?? [] }
+    set { set(newValue, forKey: "AppleLanguages") }
+  }
+
   final public subscript(key: String) -> PropertyListObject? {
     @inlinable get { return object(forKey: key) as! PropertyListObject? }
     @inlinable set { set(newValue, forKey: key) }
