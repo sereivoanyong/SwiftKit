51dccf73b1420d36923947e8a6302ef2573118d9
Sereivoan Yong <sereivoanyong@gmail.com>
2021-06-19 16:04:28 +0700

    Organize `UIEdgeInsets`

--- Sources/SwiftKit/UIKit/UIEdgeInsets.swift
+++ Sources/SwiftKit/UIKit/UIEdgeInsets.swift
@@ -21,36 +21,45 @@ extension UIEdgeInsets {
     self.init(top: top, left: 0, bottom: bottom, right: 0)
   }
   
-  @inlinable public var horizontal: CGFloat {
-    return left + right
+  @inlinable
+  public var horizontal: CGFloat {
+    left + right
   }
   
-  @inlinable public var vertical: CGFloat {
-    return top + bottom
+  @inlinable
+  public var vertical: CGFloat {
+    top + bottom
   }
   
-  @inlinable public var withoutHorizontal: UIEdgeInsets {
-    return withHorizontal(left: 0, right: 0)
+  @inlinable
+  public var withoutHorizontal: UIEdgeInsets {
+    withHorizontal(left: 0, right: 0)
   }
   
-  @inlinable public var withoutVertical: UIEdgeInsets {
-    return withVertical(top: 0, bottom: 0)
+  @inlinable
+  public var withoutVertical: UIEdgeInsets {
+    withVertical(top: 0, bottom: 0)
   }
   
-  @inlinable public func withHorizontal(left: CGFloat, right: CGFloat) -> UIEdgeInsets {
-    return UIEdgeInsets(top: top, left: left, bottom: bottom, right: right)
+  @inlinable
+  public func withHorizontal(left: CGFloat, right: CGFloat) -> UIEdgeInsets {
+    UIEdgeInsets(top: top, left: left, bottom: bottom, right: right)
   }
   
-  @inlinable public func withVertical(top: CGFloat, bottom: CGFloat) -> UIEdgeInsets {
-    return UIEdgeInsets(top: top, left: left, bottom: bottom, right: right)
+  @inlinable
+  public func withVertical(top: CGFloat, bottom: CGFloat) -> UIEdgeInsets {
+    UIEdgeInsets(top: top, left: left, bottom: bottom, right: right)
   }
-  
-  public static func + (_ lhs: UIEdgeInsets, _ rhs: UIEdgeInsets) -> UIEdgeInsets {
-    return UIEdgeInsets(top: lhs.top + rhs.top, left: lhs.left + rhs.left, bottom: lhs.bottom + rhs.bottom, right: lhs.right + rhs.right)
+}
+
+extension UIEdgeInsets: AdditiveArithmetic {
+
+  public static func + (lhs: UIEdgeInsets, rhs: UIEdgeInsets) -> UIEdgeInsets {
+    UIEdgeInsets(top: lhs.top + rhs.top, left: lhs.left + rhs.left, bottom: lhs.bottom + rhs.bottom, right: lhs.right + rhs.right)
   }
-  
-  public static func += (_ lhs: inout UIEdgeInsets, _ rhs: UIEdgeInsets) {
-    lhs = lhs + rhs
+
+  public static func - (lhs: UIEdgeInsets, rhs: UIEdgeInsets) -> UIEdgeInsets {
+    UIEdgeInsets(top: lhs.top - rhs.top, left: lhs.left - rhs.left, bottom: lhs.bottom - rhs.bottom, right: lhs.right - rhs.right)
   }
 }
 #endif
