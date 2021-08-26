20325887d9175316f3690ab15d39e55b229ce308
Sereivoan Yong <sereivoanyong@gmail.com>
2021-08-26 10:37:57 +0700

    Add static `utc` to `TimeZone`

---
+++ Sources/SwiftKit/Foundation/TimeZone.swift
@@ -0,0 +1,14 @@
+//
+//  TimeZone.swift
+//
+//  Created by Sereivoan Yong on 8/26/21.
+//
+
+#if canImport(Foundation)
+import Foundation
+
+extension TimeZone {
+
+  public static let utc = TimeZone(abbreviation: "UTC")!
+}
+#endif
