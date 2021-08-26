8c0957ebc1c773bf5dab0b80827ccbd61cd9b58c
Sereivoan Yong <sereivoanyong@gmail.com>
2021-08-26 10:55:21 +0700

    Add static `current(locale:timeZone:)` to `Calendar`

---
+++ Sources/SwiftKit/Foundation/Calendar.swift
@@ -0,0 +1,22 @@
+//
+//  Calendar.swift
+//
+//  Created by Sereivoan Yong on 8/26/21.
+//
+
+#if canImport(Foundation)
+import Foundation
+
+extension Calendar {
+
+  @inlinable
+  public static func current(locale: Locale? = nil, timeZone: TimeZone? = nil) -> Calendar {
+    var calendar = current
+    calendar.locale = locale
+    if let timeZone = timeZone {
+      calendar.timeZone = timeZone
+    }
+    return calendar
+  }
+}
+#endif
