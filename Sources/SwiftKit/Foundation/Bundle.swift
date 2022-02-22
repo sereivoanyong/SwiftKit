1d1fad2937fbe7fdc65822e911596fdcbff17e88
Sereivoan Yong <sereivoanyong@gmail.com>
2022-02-22 11:37:13 +0700

    Add instance `displayNameWithShortVersionAndVersion()` to `Bundle`

---
+++ Sources/SwiftKit/Foundation/Bundle.swift
@@ -0,0 +1,24 @@
+//
+//  Bundle.swift
+//
+//  Created by Sereivoan Yong on 2/22/22.
+//
+
+import Foundation
+
+extension Bundle {
+
+  final public func displayNameWithShortVersionAndVersion() -> String? {
+    guard let infoDictionary = infoDictionary, let displayName = infoDictionary["CFBundleDisplayName"] as? String ?? infoDictionary["CFBundleName"] as? String else {
+      return nil
+    }
+    var string = displayName
+    if let shortVersionString = infoDictionary["CFBundleShortVersionString"] as? String {
+      string += " " + shortVersionString
+    }
+    if let versionString = infoDictionary["CFBundleVersion"] as? String {
+      string += " " + versionString
+    }
+    return string
+  }
+}
