4770d4355ebb4a3cb76676359f221c09ff92ddef
Sereivoan Yong <sereivoanyong@gmail.com>
2022-04-06 11:30:03 +0700

    Add `DelayedBacked`

---
+++ Sources/PropertyWrapperKit/DelayedBacked.swift
@@ -0,0 +1,33 @@
+//
+//  DelayedBacked.swift
+//
+//  Created by Sereivoan Yong on 4/6/22.
+//
+
+import Foundation
+
+@propertyWrapper
+public struct DelayedBacked<Value, Wrapped> {
+
+  private var value: Value!
+
+  public init() {
+  }
+
+  public var wrappedValue: Wrapped {
+    get {
+      guard let value = value else {
+        fatalError("property accessed before being initialized")
+      }
+      return value as! Wrapped
+    }
+    set {
+      value = newValue as? Value
+    }
+  }
+
+  public var projectedValue: Value {
+    get { return value }
+    set { value = newValue }
+  }
+}
