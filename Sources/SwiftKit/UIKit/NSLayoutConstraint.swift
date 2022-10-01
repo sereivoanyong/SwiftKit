bebd74af39431eee0581cdfeb4449e76ce5fc672
Sereivoan Yong <sereivoanyong@gmail.com>
2022-10-01 11:13:44 +0700

    Add `deactivate()` to `Array` of `NSLayoutConstraint`

--- Sources/SwiftKit/UIKit/NSLayoutConstraint.swift
+++ Sources/SwiftKit/UIKit/NSLayoutConstraint.swift
@@ -8,22 +8,28 @@
 import UIKit
 
 extension NSLayoutConstraint {
-  
+
   public static func constraints(visualFormats: [String], options: FormatOptions = [], metrics: [String: Any]? = nil, views: [String: LayoutGuide]) -> [NSLayoutConstraint] {
     return visualFormats.flatMap { constraints(withVisualFormat: $0, options: options, metrics: metrics, views: views) }
   }
 }
 
 extension Array where Element == NSLayoutConstraint {
-  
+
+  @inlinable
   public func activate() {
     NSLayoutConstraint.activate(self)
   }
-  
+
   @discardableResult
   public func activated() -> [NSLayoutConstraint] {
     NSLayoutConstraint.activate(self)
     return self
+  }
+
+  @inlinable
+  public func deactivate() {
+    NSLayoutConstraint.deactivate(self)
   }
 }
 #endif
