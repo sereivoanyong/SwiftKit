51d7d9a3036153b3aa523bf1acbcc6c24df4fa78
Sereivoan Yong <sereivoanyong@gmail.com>
2022-11-23 18:19:44 +0700

    Improve `NSObject`

--- Sources/SwiftKit/ObjectiveC/NSObject.swift
+++ Sources/SwiftKit/ObjectiveC/NSObject.swift
@@ -5,85 +5,107 @@
 //
 
 #if canImport(ObjectiveC)
+
 import ObjectiveC
+
+extension NSObject {
+
+  @inlinable
+  public func valueIfResponds(forKey key: String) -> Any? {
+    if responds(to: Selector(key)) {
+      return value(forKey: key)
+    }
+    return nil
+  }
+
+  @inlinable
+  public func setValueIfResponds(_ value: Any?, forKey key: String) {
+    if responds(to: Selector(key)) {
+      setValue(value, forKey: key)
+    }
+  }
+}
 
 extension NSObjectProtocol {
 
-  @discardableResult
   @inlinable
+  @discardableResult
   public func with<Value>(_ keyPath: ReferenceWritableKeyPath<Self, Value>, _ value: Value) -> Self {
     self[keyPath: keyPath] = value
     return self
   }
 
-  @discardableResult
   @inlinable
+  @discardableResult
   public func configure(_ handler: (Self) -> Void) -> Self {
     handler(self)
     return self
   }
-  
+
+  @inlinable
   public func assigned(to object: inout Self?) -> Self {
     object = self
     return self
   }
-  
+
+  @inlinable
   public func assigned<Root>(to keyPath: ReferenceWritableKeyPath<Root, Self?>, on object: Root) -> Self {
     object[keyPath: keyPath] = self
     return self
   }
-  
-  @inlinable public func valueIfResponds(forKey key: String) -> Any? {
-    if responds(to: Selector(key)) {
-      return (self as! NSObject).value(forKey: key)
-    }
-    return nil
-  }
-  
-  @inlinable public func setValueIfResponds(_ value: Any?, forKey key: String) {
-    if responds(to: Selector(key)) {
-      (self as! NSObject).setValue(value, forKey: key)
-    }
-  }
-  
+
+  @inlinable
   @discardableResult
-  @inlinable public func performIfResponds(_ selector: Selector) -> Unmanaged<AnyObject>? {
+  public func performIfResponds(_ selector: Selector) -> Unmanaged<AnyObject>? {
     if responds(to: selector) {
       return perform(selector)
     }
     return nil
   }
-  
+
+  @inlinable
   @discardableResult
-  @inlinable public func performIfResponds(_ selector: Selector, with object: Any?) -> Unmanaged<AnyObject>? {
+  public func performIfResponds(_ selector: Selector, with object: Any?) -> Unmanaged<AnyObject>? {
     if responds(to: selector) {
       return perform(selector, with: object)
+    }
+    return nil
+  }
+
+  @inlinable
+  @discardableResult
+  public func performIfResponds(_ selector: Selector, with object1: Any?, with object2: Any?) -> Unmanaged<AnyObject>? {
+    if responds(to: selector) {
+      return perform(selector, with: object1, with: object2)
     }
     return nil
   }
 }
 
 #if canImport(Foundation)
+
 import Foundation
 
 @available(iOS 11.0, *)
 private var kObservationKey: Void?
 
-extension NSObjectProtocol where Self: NSObject {
-  
+extension _KeyValueCodingAndObserving where Self: NSObject {
+
   @available(iOS 11.0, *)
   public var observations: [AnyHashable: NSKeyValueObservation] {
     get { return associatedValue(forKey: &kObservationKey, default: [:]) }
     set { setAssociatedValue(newValue, forKey: &kObservationKey)}
   }
-  
-  @discardableResult
+
   @available(iOS 11.0, *)
+  @discardableResult
   public func addObservation<Value>(for keyPath: KeyPath<Self, Value>, options: NSKeyValueObservingOptions = [], changeHandler: @escaping (Self, NSKeyValueObservedChange<Value>) -> Void) -> NSKeyValueObservation {
     let observation = observe(keyPath, options: options, changeHandler: changeHandler)
     observations[keyPath] = observation
     return observation
   }
 }
+
 #endif
+
 #endif
