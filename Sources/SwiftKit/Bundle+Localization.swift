ae8bc878d4cfcb95ad7398b8575214d6b6fe8535
Sereivoan Yong <sereivoanyong@gmail.com>
2022-12-20 16:29:57 +0700

    Clean up localization (breaking changes)

--- Sources/SwiftKit/Bundle+Localization.swift
+++ Sources/SwiftKit/Bundle+Localization.swift
@@ -8,43 +8,82 @@ import Foundation
 
 extension Locale {
 
-  public fileprivate(set) static var selected = Locale(identifier: Bundle.main.selectedLocalization) {
+  /// The selected locale based on the selected localization of `Bundle.main`. It is useful for formatters.
+  public fileprivate(set) static var selected = Locale(localization: Bundle.main.selectedLocalization) {
     didSet {
-      changeHandler?(selected)
+      let selected = selected
+      for (_, handler) in autoupdatingSelectedHandlers {
+        handler(selected)
+      }
     }
   }
-  public static var changeHandler: ((Locale) -> Void)?
+
+  private static var autoupdatingSelectedHandlers: [UUID: (Locale) -> Void] = [:]
+
+  /// When the `selected` locale changes, it will reflect to the specified object at the specified key path.
+  /// This is a workaround since we cannot mimic behavior of `autoupdatingCurrent`.
+  ///
+  ///     Locale.configureAutoupdatingSelected(to: formatter, at: \.locale)
+  @discardableResult
+  public static func configureAutoupdatingSelected<T: AnyObject>(to object: T, at keyPath: ReferenceWritableKeyPath<T, Locale>) -> UUID {
+    return configureAutoupdatingSelected { [weak object] selected in
+      object?[keyPath: keyPath] = selected
+    }
+  }
+
+  /// When the `selected` locale changes, `handler` will be called.
+  /// This is a workaround since we cannot mimic behavior of `autoupdatingCurrent`.
+  ///
+  ///     Locale.configureAutoupdatingSelected { [weak formatter] locale in
+  ///       formatter?.locale = locale
+  ///     }
+  @discardableResult
+  public static func configureAutoupdatingSelected(handler: @escaping (Locale) -> Void) -> UUID {
+    let id = UUID()
+    handler(selected)
+    autoupdatingSelectedHandlers[id] = handler
+    return id
+  }
+
+  public static func unconfigureAutoupdatingSelected(_ id: UUID) {
+    autoupdatingSelectedHandlers[id] = nil
+  }
 }
 
 extension Bundle {
 
-  public static let selectedLocalizationUserDefaultsKey = "SelectedLocalization"
+  public static let selectedLocalizationsUserDefaultsKey = "BundleSelectedLocalizations"
 
   public static let selectedLocalizationDidChangeNotification = Notification.Name("BundleSelectedLocalizationDidChangeNotification")
+
+  private func setLocalization(_ localization: String, bundleIdentifier: String) {
+    let store = UserDefaults.standard
+    let selectedLocalizations = (store[Self.selectedLocalizationsUserDefaultsKey] as? NSDictionary ?? [:]).mutableCopy() as! NSMutableDictionary
+    selectedLocalizations[bundleIdentifier] = localization
+    store[Self.selectedLocalizationsUserDefaultsKey] = selectedLocalizations
+    if self == .main {
+      store["AppleLanguages"] = [localization]
+      Locale.selected = Locale(localization: localization)
+    }
+    selectedLocalizationBundle = nil
+    NotificationCenter.default.post(name: Self.selectedLocalizationDidChangeNotification, object: self)
+  }
 
   /// Default is `preferredLocalizations.first`.
   public var selectedLocalization: String! {
     get {
-      if let selectedLocalization = UserDefaults.standard.string(forKey: Self.selectedLocalizationUserDefaultsKey) {
+      guard let bundleIdentifier else { return nil }
+      if let selectedLocalization = (UserDefaults.standard[Self.selectedLocalizationsUserDefaultsKey] as? NSDictionary)?[bundleIdentifier] as? String {
         return selectedLocalization
       }
-      for localization in preferredLocalizations {
-        UserDefaults.standard.appleLanguages = [localization]
-        UserDefaults.standard.set(localization, forKey: Self.selectedLocalizationUserDefaultsKey)
-        selectedLocalizationBundle = nil
-        return localization
-      }
-      return nil
+      let localization = preferredLocalizations.first ?? "en"
+      setLocalization(localization, bundleIdentifier: bundleIdentifier)
+      return localization
     }
-    set(newLocalization) {
-      let newLocalization = newLocalization ?? preferredLocalizations.first!
-      UserDefaults.standard.appleLanguages = [newLocalization]
-      UserDefaults.standard.set(newLocalization, forKey: Self.selectedLocalizationUserDefaultsKey)
-      selectedLocalizationBundle = nil
-      if self == .main {
-        Locale.selected = Locale(identifier: newLocalization)
-      }
-      NotificationCenter.default.post(name: Self.selectedLocalizationDidChangeNotification, object: self)
+    set {
+      guard let bundleIdentifier else { return }
+      let localization = newValue ?? preferredLocalizations.first ?? "en"
+      setLocalization(localization, bundleIdentifier: bundleIdentifier)
     }
   }
 
@@ -55,7 +94,7 @@ extension Bundle {
       if let bundle = associatedObject(forKey: &Self.selectedLocalizationBundleKey) as Bundle? {
         return bundle
       }
-      if let bundle = url(forResource: selectedLocalization, withExtension: "lproj").flatMap(Bundle.init(url:)) {
+      if let url = url(forResource: selectedLocalization, withExtension: "lproj"), let bundle = Bundle(url: url) {
         setAssociatedObject(bundle, forKey: &Self.selectedLocalizationBundleKey)
         return bundle
       }
--- Sources/SwiftKit/Foundation/Locale.swift
+++ Sources/SwiftKit/Foundation/Locale.swift
@@ -10,6 +10,22 @@ import Foundation
 extension Locale {
 
   public static let enUSPOSIX: Locale = Locale(identifier: "en_US_POSIX")
+
+  public init(localization: String) {
+    let components = Self.components(fromIdentifier: localization)
+    if let languageCode = components[NSLocale.Key.languageCode.rawValue] {
+      var identifier = languageCode
+      if let scriptCode = components[NSLocale.Key.scriptCode.rawValue] {
+        identifier += "_" + scriptCode
+      }
+      if let regionCode = components[NSLocale.Key.countryCode.rawValue] {
+        identifier += "_" + regionCode
+      }
+      self.init(identifier: identifier)
+    } else {
+      self.init(identifier: localization)
+    }
+  }
   
   @available(*, deprecated, renamed: "emojiFlag(forRegionCode:)")
   public static func flagEmoji(forRegionCode regionCode: String) -> String {
