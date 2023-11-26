7396561772ca8cc6da8bba4e01005a438ba1496d
Sereivoan Yong <sereivoanyong@gmail.com>
2023-11-26 20:15:42 +0700

    Clean up `CGRect`

--- Sources/SwiftKit/CoreGraphics/CGRect.swift
+++ Sources/SwiftKit/CoreGraphics/CGRect.swift
@@ -4,40 +4,46 @@
 //  Created by Sereivoan Yong on 8/2/17.
 //
 
+#if canImport(CoreGraphics)
+
 import CoreGraphics
 
 extension CGRect {
-  
+
   public init(x: CGFloat, y: CGFloat, size: CGSize) {
     self.init(origin: CGPoint(x: x, y: y), size: size)
   }
-  
+
   public init(origin: CGPoint, width: CGFloat, height: CGFloat) {
     self.init(origin: origin, size: CGSize(width: width, height: height))
   }
-  
+
   public var x: CGFloat {
     @inlinable get { return origin.x }
     @inlinable mutating set { origin.x = newValue }
   }
-  
+
   public var y: CGFloat {
     @inlinable get { return origin.y }
     @inlinable mutating set { origin.y = newValue }
   }
-  
+
+  public var center: CGPoint {
+    return CGPoint(x: midX, y: midY)
+  }
+
   /*
   public var width: CGFloat {
     @inlinable get { return size.width }
     @inlinable mutating set { size.width = newValue }
   }
-  
+
   public var height: CGFloat {
     @inlinable get { return size.height }
     @inlinable mutating set { size.height = newValue }
   }
   */
-  
+
   public func ratioThatFits(_ rect: CGRect) -> CGFloat {
     let ratio = rect.width / width
     if height * ratio <= rect.height {
@@ -45,7 +51,7 @@ extension CGRect {
     }
     return rect.height / height
   }
-  
+
   public func rectThatFits(_ rect: CGRect) -> CGRect {
     let ratio = ratioThatFits(rect)
     let size = CGSize(width: ratio * width, height: ratio * height)
@@ -53,3 +59,5 @@ extension CGRect {
     return CGRect(origin: offset, size: size)
   }
 }
+
+#endif
