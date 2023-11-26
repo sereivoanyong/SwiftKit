c862e9c9d9520ea42805b9ddbecf1196ae0b08d1
Sereivoan Yong <sereivoanyong@gmail.com>
2023-11-26 20:13:57 +0700

    Clean up `CGPoint`

--- Sources/SwiftKit/CoreGraphics/CGPoint.swift
+++ Sources/SwiftKit/CoreGraphics/CGPoint.swift
@@ -4,39 +4,48 @@
 //  Created by Sereivoan Yong on 1/24/20.
 //
 
+#if canImport(CoreGraphics)
+
 import CoreGraphics
 
 extension CGPoint {
-  
+
+  public func distance(from point: CGPoint) -> CGFloat {
+    // http://stackoverflow.com/questions/6416101/calculate-the-distance-between-two-cgpoints
+    return sqrt(pow(point.x - x, 2) + pow(point.y - y, 2))
+  }
+
   public static func + (lhs: CGPoint, rhs: CGPoint) -> CGPoint {
     return CGPoint(x: lhs.x + rhs.x, y: lhs.y + rhs.y)
   }
-  
+
   public static func += (lhs: inout CGPoint, rhs: CGPoint) {
     lhs = lhs + rhs
   }
-  
+
   public static func - (lhs: CGPoint, rhs: CGPoint) -> CGPoint {
     return CGPoint(x: lhs.x - rhs.x, y: lhs.y - rhs.y)
   }
-  
+
   public static func -= (lhs: inout CGPoint, rhs: CGPoint) {
     lhs = lhs - rhs
   }
-  
+
   public static func * (point: CGPoint, scalar: CGFloat) -> CGPoint {
     return CGPoint(x: point.x * scalar, y: point.y * scalar)
   }
-  
+
   public static func *= (point: inout CGPoint, scalar: CGFloat) {
     point = point * scalar
   }
-  
+
   public static func / (point: CGPoint, scalar: CGFloat) -> CGPoint {
     return CGPoint(x: point.x / scalar, y: point.y / scalar)
   }
-  
+
   public static func /= (point: inout CGPoint, scalar: CGFloat) {
     point = point / scalar
   }
 }
+
+#endif
