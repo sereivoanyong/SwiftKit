f326fa5884e75f37953a4113b203143c61182c52
Sereivoan Yong <sereivoanyong@gmail.com>
2024-01-05 15:18:51 +0700

    Clean up

--- Sources/SwiftKit/Swift/Sequence.swift
+++ Sources/SwiftKit/Swift/Sequence.swift
@@ -6,52 +6,6 @@
 
 extension Sequence {
 
-  @inlinable 
-  public func filter<T>(where keyPath: KeyPath<Element, T>, equalTo value: T) -> [Element] where T: Equatable {
-    return filter({ $0[keyPath: keyPath] == value })
-  }
-
-  @inlinable 
-  public func first<T>(where keyPath: KeyPath<Element, T>, equalTo value: T) -> Element? where T: Equatable {
-    return first(where: { $0[keyPath: keyPath] == value })
-  }
-
-  /// Returns an array containing, in order, the first unique elements of the sequence that compare equally for the key path.
-  public func firstUnique<T: Hashable>(for keyPath: KeyPath<Element, T>) -> [Element] {
-    var unique = Set<T>()
-    return filter { unique.insert($0[keyPath: keyPath]).inserted }
-  }
-
-  @inlinable
-  public func map<T>(_ keyPath: KeyPath<Element, T>) -> [T] {
-    map { $0[keyPath: keyPath] }
-  }
-
-  @inlinable
-  public func flatMap<SegmentOfResult>(_ keyPath: KeyPath<Element, SegmentOfResult>) -> [SegmentOfResult.Element] where SegmentOfResult: Sequence {
-    flatMap { $0[keyPath: keyPath] }
-  }
-
-  @inlinable
-  public func compactMap<ElementOfResult>(_ keyPath: KeyPath<Element, ElementOfResult?>) -> [ElementOfResult] {
-    compactMap { $0[keyPath: keyPath] }
-  }
-
-  /// Returns a Boolean value indicating whether the sequence contains an element that satisfies the given predicate or a descendant that does.
-  public func contains<S>(where predicate: (Element) throws -> Bool, descendantsProvider: ((Element) -> S?)?) rethrows -> Bool where S: Sequence, S.Element == Element {
-    if try contains(where: predicate) {
-      return true
-    }
-    if let descendantsProvider {
-      for element in self {
-        if let descendants = descendantsProvider(element), try descendants.contains(where: predicate, descendantsProvider: descendantsProvider) {
-          return true
-        }
-      }
-    }
-    return false
-  }
-
   /// Returns the number of elements of the sequence that satisfy the given `predicate`.
   public func count(where predicate: (Element) throws -> Bool) rethrows -> Int {
     var count = 0
@@ -61,21 +15,22 @@ extension Sequence {
     return count
   }
 
-  /// Returns the number of elements of the sequence that satisfy the given `predicate` and their descendants that do.
-  public func count<S>(where predicate: (Element) throws -> Bool, descendantsProvider: ((Element) -> S?)?) rethrows -> Int where S: Sequence, S.Element == Element {
-    guard let descendantsProvider else {
-      return try count(where: predicate)
-    }
-    var count = 0
-    for element in self {
-      if try predicate(element) {
-        count += 1
-      }
-      if let descendants = descendantsProvider(element) {
-        count += try descendants.count(where: predicate, descendantsProvider: descendantsProvider)
-      }
-    }
-    return count
+  public func count<T: Equatable>(where transform: (Element) throws -> T, equalTo value: T) rethrows -> Int {
+    return try count { try transform($0) == value }
+  }
+
+  public func filter<T: Equatable>(where transform: (Element) throws -> T, equalTo value: T) rethrows -> [Element] {
+    return try filter { try transform($0) == value }
+  }
+
+  public func first<T: Equatable>(where transform: (Element) throws -> T, equalTo value: T) rethrows -> Element? {
+    return try first { try transform($0) == value }
+  }
+
+  /// Returns an array containing, in order, the first unique elements of the sequence that compare equally for the key path.
+  public func firstUnique<T: Hashable>(where transform: (Element) throws -> T) rethrows -> [Element] {
+    var unique: Set<T> = []
+    return try filter { unique.insert(try transform($0)).inserted }
   }
 
   /// Returns the first element of the `type` of the sequence that satisfies the given `predicate`.
@@ -84,9 +39,9 @@ extension Sequence {
   ///     if let textField = first(of: UITextField.self, where: { $0.hasText }) {
   ///         print("The first non-empty text field is \(textField).")
   ///     }
-  public func first<T>(of type: T.Type, where predicate: ((T) throws -> Bool)? = nil) rethrows -> T? {
+  public func first<T>(of type: T.Type, where predicate: (T) throws -> Bool = { _ in true }) rethrows -> T? {
     for element in self {
-      if let match = element as? T, try predicate?(match) ?? true {
+      if let match = element as? T, try predicate(match) {
         return match
       }
     }
@@ -96,25 +51,22 @@ extension Sequence {
   /// Returns the first element of the `type` of the sequence that satisfies the given `predicate` or first descendant that does.
   ///
   ///     let view = UIView()
-  ///     if let textField = first(of: UITextField.self, where: { $0.hasText }, descendantsProvider: { $0.subviews }) {
+  ///     if let textField = first(of: UITextField.self, where: { $0.hasText }, descendants: \.subviews) {
   ///         print("The first non-empty descendant text field is \(textField).")
   ///     }
-  public func first<T, S>(of type: T.Type, where predicate: ((T) throws -> Bool)? = nil, descendantsProvider: ((Element) -> S?)?) rethrows -> T? where S: Sequence, S.Element == Element {
+  public func first<T>(of type: T.Type, where predicate: (T) throws -> Bool = { _ in true }, descendants: (Element) -> some Sequence<Element>) rethrows -> T? {
     if let first = try first(of: type, where: predicate) {
       return first
     }
-    guard let descendantsProvider else {
-      return nil
-    }
     for element in self {
-      if let descendants = descendantsProvider(element), let descendantMatch = try descendants.first(of: T.self, where: predicate, descendantsProvider: descendantsProvider) {
-        return descendantMatch
+      if let first = try descendants(element).first(of: T.self, where: predicate, descendants: descendants) {
+        return first
       }
     }
     return nil
   }
   
-  public func dictionary<Key, Value>(_ transform: (Element) throws -> (Key, Value)?) rethrows -> [Key: Value] where Key: Hashable {
+  public func dictionary<Key: Hashable, Value>(_ transform: (Element) throws -> (Key, Value)?) rethrows -> [Key: Value] {
     var dictionary: [Key: Value] = [:]
     for element in self {
       if let (key, value) = try transform(element) {
@@ -124,19 +76,6 @@ extension Sequence {
     return dictionary
   }
   
-  public func count<Value>(_ initialValue: Value, _ nextPartialValue: (Value, Element) -> Value, while predicate: (Value) -> Bool) -> Int {
-    var count: Int = 0
-    var value = initialValue
-    for element in self {
-      if predicate(value) {
-        return count
-      }
-      value = nextPartialValue(value, element)
-      count += 1
-    }
-    return count
-  }
-  
   public var array: [Element] {
     return [Element](self)
   }
@@ -144,8 +83,11 @@ extension Sequence {
   public var contiguousArray: ContiguousArray<Element> {
     return ContiguousArray<Element>(self)
   }
+}
 
-  public func sum() -> Element where Element: AdditiveArithmetic {
-    reduce(.zero, +)
+extension Sequence where Element: Hashable {
+
+  public var set: Set<Element> {
+    return Set<Element>(self)
   }
 }
--- Sources/SwiftKit/Swift/Swift.swift
+++ Sources/SwiftKit/Swift/Swift.swift
@@ -59,36 +59,72 @@ public func isEqual<T, U: Equatable>(_ lhs: T, _ rhs: T, at keyPath: KeyPath<T, 
   return lhs[keyPath: keyPath] == rhs[keyPath: keyPath]
 }
 
-/// Returns the first value for the key path of the type on the value that satisfies the given predicate.
+/// Returns the first value.
 ///
 ///     let view = UIView()
-///     if let firstSuperScrollview = first(\.superview, of: UIScrollView.self, on: view) {
+///     if let firstSuperScrollview = first(view, of: UIScrollView.self, next: \.superview) {
 ///         print("The first super scroll view is \(firstSuperScrollview).")
 ///     }
 @inlinable
-public func first<Value, T>(_ keyPath: KeyPath<Value, Value?>, of type: T.Type, on value: Value, where predicate: ((T) -> Bool)? = nil) -> T? {
-  if let value = value[keyPath: keyPath] {
-    if let value = value as? T, predicate?(value) ?? true {
-      return value
-    }
-    return first(keyPath, of: type, on: value, where: predicate)
+public func first<T, U>(_ value: T?, of type: U.Type, next: (T) throws -> T?) rethrows -> U? {
+  guard let value else { return nil }
+  if let value = value as? U {
+    return value
   }
-  return nil
+  let nextValue = try next(value)
+  return try first(nextValue, of: type, next: next)
 }
 
-/// Returns the first value for the key path on the value that satisfies the given predicate.
+/// Returns the first value.
 ///
 ///     let view = UIView()
-///     if let firstRedSuperview = first(\.superview, on: view, where: { $0.backgroundColor == .systemRed }) {
-///         print("The first red superview is \(firstRedSuperview).")
+///     if let firstRedSuperScrollview = first(view, of: UIScrollView.self, where: { $0.backgroundColor == .red }, next: \.superview) {
+///         print("The first red super scroll view is \(firstRedSuperScrollview).")
 ///     }
 @inlinable
-public func first<Value>(_ keyPath: KeyPath<Value, Value?>, on value: Value, where predicate: ((Value) -> Bool)? = nil) -> Value? {
-  if let value = value[keyPath: keyPath] {
-    if predicate?(value) ?? true {
-      return value
-    }
-    return first(keyPath, on: value, where: predicate)
+public func first<T, U>(_ value: T?, of type: U.Type, where predicate: (U) throws -> Bool = { _ in true }, next: (T) throws -> T?) rethrows -> U? {
+  guard let value else { return nil }
+  if let value = value as? U, try predicate(value) {
+    return value
   }
-  return nil
+  let nextValue = try next(value)
+  return try first(nextValue, of: type, where: predicate, next: next)
+}
+
+/// Returns the first value.
+///
+///     let view = UIView()
+///     if let firstRedSuperScrollview = first(view, of: UIScrollView.self, where: \.backgroundColor, equalTo: .red, next: \.superview) {
+///         print("The first red super scroll view is \(firstRedSuperScrollview).")
+///     }
+@inlinable
+public func first<T, U, V: Equatable>(_ value: T?, of type: U.Type, where transform: (U) throws -> V, equalTo target: V, next: (T) throws -> T?) rethrows -> U? {
+  return try first(value, of: type, where: { try transform($0) == target }, next: next)
+}
+
+/// Returns the first value.
+///
+///     let view = UIView()
+///     if let firstRedSuperview = first(view, where: { $0.backgroundColor == .red }, next: \.superview) {
+///         print("The first red super view is \(firstRedSuperview).")
+///     }
+@inlinable
+public func first<T>(_ value: T?, where predicate: (T) throws -> Bool, next: (T) throws -> T?) rethrows -> T? {
+  guard let value else { return nil }
+  if try predicate(value) {
+    return value
+  }
+  let nextValue = try next(value)
+  return try first(nextValue, where: predicate, next: next)
+}
+
+/// Returns the first value.
+///
+///     let view = UIView()
+///     if let firstRedSuperview = first(view, where: \.backgroundColor, equalTo: .red, next: \.superview) {
+///         print("The first red super view is \(firstRedSuperview).")
+///     }
+@inlinable
+public func first<T, U: Equatable>(_ value: T?, where transform: (T) throws -> U?, equalTo target: U, next: (T) throws -> T?) rethrows -> T? {
+  return try first(value, where: { try transform($0) == target }, next: next)
 }
