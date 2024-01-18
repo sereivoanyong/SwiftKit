27bfee491fbcd38c2308b1a063cdc3fcb2576e28
Sereivoan Yong <sereivoanyong@gmail.com>
2024-01-18 13:58:40 +0700

    Refactor attributed strings

--- Sources/SwiftKit/Foundation/NSAttributedString.swift
+++ Sources/SwiftKit/Foundation/NSAttributedString.swift
@@ -4,37 +4,72 @@
 //  Created by Sereivoan Yong on 8/22/20.
 //
 
+#if canImport(Foundation)
+
 import Foundation
 
 extension NSAttributedString {
-  
+
   public static func space<Number>(_ width: Number) -> NSAttributedString where Number: _ObjectiveCBridgeable, Number._ObjectiveCType: NSNumber {
     var attchmentCharacter: unichar = 0xFFFC // NSTextAttachment.character
     let nonPrintableString = String(utf16CodeUnits: &attchmentCharacter, count: 1) // This can be anything non-printable
     return NSAttributedString(string: nonPrintableString, attributes: [.kern: width._bridgeToObjectiveC()])
   }
-  
+
+  @inlinable
   public static func + (lhs: NSAttributedString, rhs: NSAttributedString) -> NSMutableAttributedString {
     let result = NSMutableAttributedString(attributedString: lhs)
     result.append(rhs)
     return result
   }
-  
+
+  @inlinable
   public static func + (lhs: NSAttributedString, rhs: String) -> NSMutableAttributedString {
-    return lhs + NSAttributedString(string: rhs)
+    let result = NSMutableAttributedString(attributedString: lhs)
+    result.append(NSAttributedString(string: rhs))
+    return result
   }
 }
 
-extension Collection where Element: NSAttributedString, Index == Int {
-  
-  public func joined(separator: String, attributes: [NSAttributedString.Key: Any]? = nil) -> NSMutableAttributedString {
-    let attributedSeparator = NSAttributedString(string: separator, attributes: attributes)
-    return enumerated().reduce(into: NSMutableAttributedString()) { result, pair in
-      let (index, attributedString) = pair
-      result.append(attributedString)
-      if index < endIndex - 1 {
-        result.append(attributedSeparator)
-      }
-    }
+extension String {
+
+  @inlinable
+  public func attributed(attributes: [NSAttributedString.Key: Any]? = nil) -> NSMutableAttributedString {
+    return NSMutableAttributedString(string: self, attributes: attributes)
   }
 }
+
+extension Sequence {
+
+  // https://github.com/apple/swift/blob/dd3f6ef2a67a6b6caf3c71f93098b164550d2756/stdlib/public/core/String.swift#L788
+  @inlinable
+  public func joined(separator: NSAttributedString) -> NSMutableAttributedString where Element: NSAttributedString {
+    let result = NSMutableAttributedString()
+    if separator.length == 0 {
+      for element in self {
+        result.append(element)
+      }
+      return result
+    }
+
+    var iterator = makeIterator()
+    if let first = iterator.next() {
+      result.append(first)
+      while let next = iterator.next() {
+        result.append(separator)
+        result.append(next)
+      }
+    }
+    return result
+  }
+
+  public func attributedJoined(attributes: [NSAttributedString.Key: Any]? = nil, attributedSeparator: NSAttributedString) -> NSMutableAttributedString where Element == String {
+    return map { NSAttributedString(string: $0, attributes: attributes) }.joined(separator: attributedSeparator)
+  }
+
+  public func attributedJoined(separator: String, attributes: [NSAttributedString.Key: Any]? = nil) -> NSMutableAttributedString where Element == String {
+    return attributedJoined(attributes: attributes, attributedSeparator: NSAttributedString(string: separator, attributes: attributes))
+  }
+}
+
+#endif
--- Sources/SwiftKit/Foundation/NSMutableAttributedString.swift
+++ Sources/SwiftKit/Foundation/NSMutableAttributedString.swift
@@ -4,15 +4,21 @@
 //  Created by Sereivoan Yong on 1/24/20.
 //
 
+#if canImport(Foundation)
+
 import Foundation
 
 extension NSMutableAttributedString {
-  
+
+  @inlinable
   public static func += (lhs: NSMutableAttributedString, rhs: NSAttributedString) {
     lhs.append(rhs)
   }
-  
+
+  @inlinable
   public static func += (lhs: NSMutableAttributedString, rhs: String) {
-    lhs += NSAttributedString(string: rhs)
+    lhs.append(NSAttributedString(string: rhs))
   }
 }
+
+#endif
