95e5e01f686beb94ed5eebfb7f2c10495cd8e43f
Sereivoan Yong <sereivoanyong@gmail.com>
2024-11-12 11:45:35 +0700

    Add `UIImageView`'s convenience init

--- Sources/SwiftKit/UIKit/UIImageView.swift
+++ Sources/SwiftKit/UIKit/UIImageView.swift
@@ -7,14 +7,24 @@
 import UIKit
 
 extension UIImageView {
+
+  @inlinable
+  public convenience init(image: UIImage? = nil, highlightedImage: UIImage? = nil, contentMode: ContentMode, tintColor: UIColor? = nil) {
+    self.init(frame: .zero)
+    self.image = image
+    self.highlightedImage = highlightedImage
+    self.contentMode = contentMode
+    self.tintColor = tintColor
+  }
   
-  public static func templateRendered(image: UIImage, tintColor: UIColor) -> UIImageView {
+  public static func templateRendered(image: UIImage, contentMode: ContentMode, tintColor: UIColor? = nil) -> UIImageView {
     let imageView = UIImageView()
     if image.renderingMode == .alwaysTemplate {
       imageView.image = image
     } else {
       imageView.image = image.withRenderingMode(.alwaysTemplate)
     }
+    imageView.contentMode = contentMode
     imageView.tintColor = tintColor
     return imageView
   }
