aae0e28847aa1dcbfe143a402a89864aa59d6e65
Sereivoan Yong <sereivoanyong@gmail.com>
2025-03-18 16:49:39 +0700

    Refactor configurations

--- Sources/UIKitUtilities/Views/ContentConfiguration/Object/ObjectContentView.swift
+++ Sources/UIKitUtilities/Views/ContentConfiguration/Object/ObjectContentView.swift
@@ -9,50 +9,21 @@ import UIKit
 import SwiftKit
 
 @available(iOS 14.0, *)
-@MainActor public protocol ObjectContentView<Object>: UIView, UIContentView {
+@MainActor public protocol ObjectContentView<Object>: ContentView where Configuration == ObjectContentConfiguration<Object> {
 
-  associatedtype Object
-
-  @MainActor var objectConfiguration: ObjectContentConfiguration<Object> { get set }
+  associatedtype Object: AnyObject
 
   @MainActor func configure(_ object: Object)
 }
 
-private var __configurationKey: Void?
-
 @available(iOS 14.0, *)
 extension ObjectContentView {
 
-  @MainActor public var __configuration: UIContentConfiguration? {
-    get { return associatedValue(forKey: &__configurationKey, with: self) }
-    set { setAssociatedValue(newValue, forKey: &__configurationKey, with: self) }
+  public var configurationObject: Object! {
+    return _configurationIfSet?.object
   }
 
-  @MainActor public var configuration: UIContentConfiguration {
-    get { return __configuration! }
-    set {
-      __configuration = newValue
-      if let newValue = newValue as? ObjectContentConfiguration<Object> {
-        configure(newValue.object)
-      }
-    }
-  }
-
-  @inlinable
-  @MainActor public var objectConfiguration: ObjectContentConfiguration<Object> {
-    get { return __configuration as! ObjectContentConfiguration<Object> }
-    set {
-      __configuration = newValue
-      configure(newValue.object)
-    }
-  }
-
-  @inlinable
-  public var object: Object {
-    return objectConfiguration.object
-  }
-
-  @MainActor public func supports(_ configuration: UIContentConfiguration) -> Bool {
-    return configuration is ObjectContentConfiguration<Object>
+  @MainActor public func configure(_ configuration: ObjectContentConfiguration<Object>) {
+    configure(configuration.object)
   }
 }
