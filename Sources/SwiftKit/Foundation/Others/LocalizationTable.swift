bd876ef020c92b15cc64665a63bbb60f8da90ca9
Sereivoan Yong <sereivoanyong@gmail.com>
2025-09-02 18:41:06 +0700

    Add `LocalizationTable`

---
+++ Sources/SwiftKit/Foundation/Others/LocalizationTable.swift
@@ -0,0 +1,17 @@
+//
+//  LocalizationTable.swift
+//  SwiftKit
+//
+//  Created by Sereivoan Yong on 9/2/25.
+//
+
+import Foundation
+
+public struct LocalizationTable: StringRepresentable {
+
+  public let rawValue: String
+
+  public init(_ rawValue: String) {
+    self.rawValue = rawValue
+  }
+}
