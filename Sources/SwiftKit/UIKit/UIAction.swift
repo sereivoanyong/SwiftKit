e71bb1cfb3c3de9c5928cbe94d2a54684f142941
Sereivoan Yong <sereivoanyong@gmail.com>
2026-01-11 00:16:52 +0700

    Add `Configuration` to `UIAction`

--- Sources/SwiftKit/UIKit/UIAction.swift
+++ Sources/SwiftKit/UIKit/UIAction.swift
@@ -14,4 +14,22 @@ extension UIAction {
     let handler = value(forKey: "handler") as AnyObject
     return unsafeBitCast(handler, to: Block.self)
   }
+
+  public struct Configuration: Hashable, Sendable {
+
+    public var title: String
+    public var subtitle: String?
+    public var image: UIImage?
+
+    public init(title: String, subtitle: String? = nil, image: UIImage? = nil) {
+      self.title = title
+      self.subtitle = subtitle
+      self.image = image
+    }
+  }
+
+  @available(iOS 15.0, *)
+  public convenience init(configuration: Configuration, identifier: UIAction.Identifier? = nil, discoverabilityTitle: String? = nil, attributes: UIMenuElement.Attributes = [], state: UIMenuElement.State = .off, handler: @escaping UIActionHandler) {
+    self.init(title: configuration.title, subtitle: configuration.subtitle, image: configuration.image, identifier: identifier, discoverabilityTitle: discoverabilityTitle, attributes: attributes, state: state, handler: handler)
+  }
 }
