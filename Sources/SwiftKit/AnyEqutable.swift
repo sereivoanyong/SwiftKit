7bd0f4b78ae5a3d533ce3e6cf575928beaede143
Sereivoan Yong <sereivoanyong@gmail.com>
2026-01-19 00:41:05 +0700

    Fix `AnyEquatable`

--- Sources/SwiftKit/AnyEqutable.swift
+++ Sources/SwiftKit/AnyEqutable.swift
@@ -1,46 +1,64 @@
 //
 //  AnyEquatable.swift
+//  SwiftKit
 //
 //  Created by Sereivoan Yong on 10/29/17.
 //
 
 // See https://github.com/apple/swift/blob/main/stdlib/public/core/AnyHashable.swift
 
+public protocol _HasCustomAnyEquatableRepresentation {
+
+  __consuming func _toCustomAnyEquatable() -> AnyEquatable?
+}
+
 @usableFromInline
 internal protocol _AnyEquatableBox {
-  
+
   var _base: Any { get }
   func _unbox<T: Equatable>() -> T?
-  
+
   /// Determine whether values in the boxes are equivalent.
   ///
   /// - Precondition: `self` and `box` are in canonical form.
   /// - Returns: `nil` to indicate that the boxes store different types, so
   ///   no comparison is possible. Otherwise, contains the result of `==`.
   func _isEqual(to box: _AnyEquatableBox) -> Bool?
+
+  func _downCastConditional<T>(into result: UnsafeMutablePointer<T>) -> Bool
 }
 
 internal struct _ConcreteEquatableBox<Base: Equatable>: _AnyEquatableBox {
-  
+
   internal let _baseEquatable: Base
-  
+
   internal init(_ base: Base) {
     self._baseEquatable = base
   }
-  
+
   internal var _base: Any {
     return _baseEquatable
   }
-  
+
   internal func _unbox<T: Equatable>() -> T? {
     return (self as _AnyEquatableBox as? _ConcreteEquatableBox<T>)?._baseEquatable
   }
-  
+
   internal func _isEqual(to rhs: _AnyEquatableBox) -> Bool? {
-    if let rhs: Base = rhs._unbox() {
-      return _baseEquatable == rhs
+    if let rhsBase: Base = rhs._unbox() {
+      return _baseEquatable == rhsBase
+    }
+    var rhsBase: Base?
+    if rhs._downCastConditional(into: &rhsBase) {
+      return _baseEquatable == rhsBase
     }
     return nil
+  }
+
+  internal func _downCastConditional<T>(into result: UnsafeMutablePointer<T>) -> Bool {
+    guard let value = _baseEquatable as? T else { return false }
+    unsafe result.initialize(to: value)
+    return true
   }
 }
 
@@ -55,16 +73,21 @@ internal struct _ConcreteEquatableBox<Base: Equatable>: _AnyEquatableBox {
 /// compare as equal. For example, `AnyEquatable(42)` compares as equal to
 /// `AnyEquatable(42 as NSNumber)`.
 public struct AnyEquatable {
-  
+
   internal let _box: _AnyEquatableBox
-  
+
   /// Creates a type-erased equatable value that wraps the given instance.
   ///
   /// - Parameter base: An equatable value to wrap.
   public init<E: Equatable>(_ base: E) {
+    if let custom = (base as? _HasCustomAnyEquatableRepresentation)?._toCustomAnyEquatable() {
+      self = custom
+      return
+    }
+
     _box = _ConcreteEquatableBox(base)
   }
-  
+
   /// The value wrapped by this instance.
   ///
   /// The `base` property can be cast back to its original type using one of
@@ -81,29 +104,36 @@ public struct AnyEquatable {
 }
 
 extension AnyEquatable: Equatable {
-  
+
   public static func == (lhs: AnyEquatable, rhs: AnyEquatable) -> Bool {
     return lhs._box._isEqual(to: rhs._box) ?? false
   }
 }
 
 extension AnyEquatable: CustomStringConvertible {
-  
+
   public var description: String {
     return String(describing: base)
   }
 }
 
 extension AnyEquatable: CustomDebugStringConvertible {
-  
+
   public var debugDescription: String {
     return "AnyEquatable(" + String(reflecting: base) + ")"
   }
 }
 
 extension AnyEquatable: CustomReflectable {
-  
+
   public var customMirror: Mirror {
     return Mirror(self, children: ["value": base])
+  }
+}
+
+extension AnyEquatable: _HasCustomAnyEquatableRepresentation {
+
+  __consuming public func _toCustomAnyEquatable() -> AnyEquatable? {
+    return self
   }
 }
